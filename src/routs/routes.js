cc50fe5a155d68c9d3cf455fe493af694acef199
Viktoriia <maksymovychvika@gmail.com>
2021-11-03 19:01:08 +0200

    handle commited

--- src/routs/routes.js
+++ src/routs/routes.js
@@ -2,12 +2,13 @@ import LogIn from "../components/Pages/logIn/LogIn";
 import Error from "../components/Pages/error/Error";
 import Movies from "../components/Pages/Movies/Movies";
 import Session from "../components/Pages/Session/Session";
+import Profile from "../components/Pages/Profile/Profile";
 
 export const routes = [
   { path: "/movie/:id", component: "Movie", isPrivate: true },
   { path: "/movies", component: Movies, isPrivate: true },
   { path: "/favorits", component: "favorits", isPrivate: true },
-  { path: "/profile", component: "profile", isPrivate: true },
+  { path: "/profile", component: Profile, isPrivate: true },
   { path: "/session", component: Session },
   { path: "/login", component: LogIn },
   { path: "/error", component: Error },
